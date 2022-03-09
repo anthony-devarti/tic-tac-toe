1feea31846096322544f32d809a71575afffdd49
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-09 18:42:57 +0000

    need to stop and work through the tutorial again

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 class Model {
     constructor() {
         this.board=[' ',' ',' ', ' ',' ',' ', ' ',' ',' '];
+        this.currentPlayer = 'x';
+        //Wincon: array of 8 arrays that are winning board states
+        Endgame: false //true when game is over
+        TurnCounter: 0 //(keeps track of turn number)
     }
-  }
+    
+}
   
   
   //View
@@ -22,7 +27,14 @@ class Model {
       //this.input.type = 'text'
       //this.input.placeholder = 'Add todo'
       //this.input.name = 'todo'
-  
+      this.board = this.createElement('h2')
+      
+      handleAddMark = (currentPlayer) => {
+        this.model.addMark(currentPlayer)
+      }
+      let b0 = this.getElementbyID('b0').innerText='test'
+      b0.innerText = 'test'
+
       this.restartButton = this.createElement('button')
       this.restartButton.textContent = 'Restart'
   
@@ -51,11 +63,30 @@ class Model {
   }
   
   
-  class Controller {
+class Controller {
     constructor(model, view) {
-      this.model = model
-      this.view = view
+        this.model = model
+        this.view = view
     }
-  }
-  
-  const app = new Controller(new Model(), new View())
+
+        onClick(){
+            console.log('clicking')
+            //Set state of board (in setters for model)
+            //i is the clicked tile
+            //let board[i] = currentplayer
+        //updateView(){
+                //show symbols on each tile according to the current state of the board object
+        }
+        //remove the event listener
+        clearListener()
+            //remove the event listener associated with that specific tile
+            //apply css class "clicked" to make it not look like a button anymore
+        wincons()
+            //written out in sandbox
+            
+        }
+            
+
+
+
+        const app = new Controller(new Model(), new View())
