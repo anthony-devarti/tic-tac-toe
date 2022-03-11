69f65288b811e293fe5778947c3794f7870822ab
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-11 14:40:17 +0000

    cleaning up and pushing to main in anticiapation of code freeze

--- main.js
+++ main.js
@@ -46,12 +46,10 @@ class View {
         this.title.textContent = 'Tic-Tac-Toe'
 
         this.board = this.createElement('board')
-        this.board.style.width = '400px'
-        this.board.style.height = '400px'
 
         this.restartButton = this.createElement('button', 'reset')
         this.restartButton.id = 'resetButton'
-        this.restartButton.textContent = 'reset'
+        this.restartButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/><path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/></svg>'
 
         this.row = this.createElement('div', 'row')
         
@@ -234,7 +232,7 @@ class Controller {
     }
     
 }
-//makes the first app
+//makes the first instance
 let app = new Controller(new Model(), new View())
 
   
